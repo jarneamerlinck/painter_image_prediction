37504ec7599cb0b189ca2d57eb2d8f172edb1359
jarneamerlinck <jarneamerlinck@gmail.com>
2023-01-08 10:59:49 +0100

    Added documentation
    Documentation format is google

--- helpers/mlflow_helpers.py
+++ helpers/mlflow_helpers.py
@@ -24,9 +24,34 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 
 class MlflowCallback(keras.callbacks.Callback):
+    """This class is used to log training and validation loss and another metric to mlflow
+    
+    Needs to be passed to the model with other callbacks.
+    Model fit needs to be inside of mlflow run
+    In MlflowController this is possible in the _set_train_options() method
+    
+    Args:
+            metric (str): Name of the metric mlflow needs to log
+            
+    Examples:
+        >>> callback = [MlflowCallback("accuracy")]
+        >>> model.fit(
+                train_dataset, 
+                batch_size=batch_size, epochs=epochs, 
+                validation_data=(val_dataset), 
+                callbacks=callback
+                )
+
+    """
     def __init__(self, metric:str):
         self.metric = metric
     def on_epoch_end(self, epoch, logs=None):
+        """logs after each epoch
+
+        Args:
+            epoch (_type_): Numberof epoch
+            logs (_type_, optional): logs, Defaults to None.
+        """
         mlflow.log_metrics({
            "loss": logs["loss"],
            self.metric:
@@ -37,24 +62,76 @@ class MlflowCallback(keras.callbacks.Callback):
     })
 
 class Mlflow_controller(ABC):
-    """
-    This class is used to deploy an model to mlflow
-    Atributes
-    ----------
-    nr:  int
-        number of hey
-    class_var: str
-        class string info
+    """Absolute class to log model runs to mlflow
+    Args:
+            experiment_name (str): Name of the mlflow experiment
+            model_name (str): Name of the model
+            version (str): Version of the model
+            
+    Attributes:
+        FEATURES_PATH (str): Path to feature file
+        LABELS_PATH (str): Path to label file
+        ENCODER_PICKLE (str): Path to encoder file
+        OUTPUT_FOLDER (str): Path to output folder
+        BACKUP_FOLDER (str): Path to backup folder
+        PREPROCESSING_FOLDER (str): Path to preprocessing folder
+        DOTENV_FILE (str): Path to dotenv file
+    Examples:
+        
+        >>> import tensorflow as tf
+        >>> from tensorflow import keras
+        >>> from tensorflow.python.keras.callbacks import ModelCheckpoint
+        >>> from tensorflow.keras import layers
+        >>> import mlflow
 
-    Methods
-    -------
-    hey() -> string:
-        returns  hey
+        >>> from helpers.mlflow_helpers import *
 
-    Examples
-    -----
-    >>> example = Example_class()
-    >>> example.hey()
+        >>> class Controller(Mlflow_controller):
+                def _build_model(self, input_shape, output_shape: int):
+                    inputs = keras.Input(shape=input_shape)
+                    x = inputs        
+                    x = layers.Flatten()(x)
+                    outputs = layers.Dense(output_shape, activation="softmax")(x)
+                    model = keras.Model(inputs=inputs, outputs=outputs)
+                    model.compile(loss="categorical_crossentropy",optimizer="rmsprop",metrics=["accuracy"])
+                    return model
+
+                def load_features(self):
+                    self.batch_size = 64
+                    self.image_shape = (180, 180)
+                    self.train_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+                        f"{PREPROCESSING_FOLDER}/train", image_size=self.image_shape, batch_size=self.batch_size, label_mode='categorical'
+                    )
+                    self.test_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+                        f"{PREPROCESSING_FOLDER}/test", image_size=self.image_shape, batch_size=self.batch_size, label_mode='categorical' 
+                    )
+                    self.val_dataset = tf.keras.preprocessing.image_dataset_from_directory(
+                        f"{PREPROCESSING_FOLDER}/val", image_size=self.image_shape, batch_size=self.batch_size, label_mode='categorical' 
+                    )
+
+
+                def _set_train_options(self):
+                    self.uses_datasets = True
+                    self.epochs = 30
+                    filename = "painter_baseline.keras"
+                    self.callback = [keras.callbacks.ModelCheckpoint(filename, save_best_only=True), MlflowCallback("accuracy")]
+                    self.input_shape = (180, 180, 3)
+                    self.labels = len(self.train_dataset.class_names)
+
+                def mlflow_log(self):
+                    t_loss, t_accuracy = self.model.evaluate(self.test_dataset)
+                    mlflow.log_metrics({"test_loss": t_loss,"test_accuracy": t_accuracy})
+                    mlflow.log_artifact("train.py", artifact_path=self.BACKUP_FOLDER)
+                def classification_report(self):
+                    self.plot_loss()
+                    self.save_to_mlflow()
+        # Use the class
+        >>> experiment = "default"
+        >>> model_name = "demo_model"
+        >>> model_version = "001"
+        >>> mlflow_controller = Controller(experiment, model_name, model_version)
+
+        >>> mlflow_controller()
     """
     FEATURES_PATH = "data/output/features.csv"
     LABELS_PATH = "data/output/labels.csv"
@@ -82,14 +159,37 @@ class Mlflow_controller(ABC):
         print(f"Experiment {self._experiment_name} as active")
 
     def log_history(self, history) -> None:
-        """Log history
+        """Saves history
         """
         self._history = history
         
     def predict(self, x_test):
+        """Predicts for X
+
+        Args:
+            x_test (numpy array): features
+
+        Returns:
+           numpy array: predicted labels
+        """
         return self.model.predict(x_test)
     
     def classification_report(self, x_test, y_test):
+        """creates the classification_report (only without datasets)
+        Is called in train()
+        Override when using datasets
+        ```
+        def classification_report(self):
+            self.plot_loss()
+            self.save_to_mlflow()
+        ```
+        Args:
+            x_test (_type_): _description_
+            y_test (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
         pred_test = self.model.predict(x_test)
         if hasattr(self,'_encoder'):
             y_pred = self._encoder.inverse_transform(pred_test)
@@ -108,6 +208,9 @@ class Mlflow_controller(ABC):
         return report
 
     def plot_loss(self):
+        """Plots Training and validation accuracy and loss 
+        Saves the plot to mlflow
+        """
         
         plt.rcParams['figure.dpi'] = 160  # figure size vergroten
         accuracy = self._history.history["accuracy"]
@@ -131,6 +234,8 @@ class Mlflow_controller(ABC):
         mlflow.log_figure(fig, f"{self.OUTPUT_FOLDER}/plot_loss.png")
         
     def save_to_mlflow(self):
+        """Logs encoder and model shape to mlflow
+        """
         self.__model_summary_to_MLFlow()
         try:
             save_encoder(self._encoder, self.ENCODER_PICKLE)
@@ -145,6 +250,8 @@ class Mlflow_controller(ABC):
             print("error when getting model plot")
     
     def __model_summary_to_MLFlow(self):
+        """Logs metrics about the model to mlflow
+        """
         stringlist = []
         self.model.summary(print_fn=lambda x: stringlist.append(x))
         summ_string = "\n".join(stringlist)
@@ -177,17 +284,14 @@ class Mlflow_controller(ABC):
             number_of_current_layer+=1
 
     def __call__(self):
-        """Runs mlflow
+        """Call of the class that runs mlflow
         """
         self._mlflow_run()
 
     def train(self):
+        """Trains the model, logs history and creates the classification_report 
+        """
         if self.uses_datasets:
-            # print(self.train_dataset, type(self.train_dataset))
-            # print(self.val_dataset, type(self.val_dataset))
-            # print(self.batch_size, type(self.batch_size))
-            # print(self.epochs, type(self.epochs))
-            # print(self.callback, type(self.callback))
             history=self.model.fit(
                 self.train_dataset, 
                 batch_size=self.batch_size, epochs=self.epochs, 
@@ -209,12 +313,28 @@ class Mlflow_controller(ABC):
             report = self.classification_report(self.x_test, self.y_test)
         
     def model_summary(self):
+        """Prints the model summary
+        """
         self.model.summary()
 
     def set_encoder(self, encoder):
+        """Saves encoder
+        Needs to be used in the child class
+
+        Args:
+            encoder (_type_): Encoder for the labels
+        """
         self._encoder = encoder
     
     def _mlflow_run(self):
+        """Runs mlflow 
+        Sequence of used methods
+        1. load_features()
+        2. _set_train_options()
+        3. _build_model()
+        4. train()
+        5. mlflow_log()
+        """
         self._mlflow_setup()
         
         with mlflow.start_run(run_name=f'{self._model_name}_{self._model_version}') as run:
@@ -234,43 +354,62 @@ class Mlflow_controller(ABC):
 
     
     def mlflow_log(self):
+        """This function is used to log artifacts, tags or metrics to mlflow after the training process
+        """
+        
         pass
         
     @abstractmethod
     def _build_model(self)-> keras.Model:
+        """This function makes, compiles and returns the keras model
+
+        Returns:
+            keras.Model: Model to be trained
+        """
         pass
     
     @abstractmethod
     def load_features(self):
-        """loads data/features
-    needs to set following variables
-    - self.x_train
-    - self.x_val
-    - self.x_test
-    - self.y_train
-    - self.y_val
-    - self.y_test
-    
-
-    """
+        """Loads data/features for the training process
+        Needs to set the following object variables
+        - x_train
+        - x_val
+        - x_test
+        - y_train
+        - y_val
+        - y_test
+        Or (set self.uses_datasets = True)
+        - train_dataset
+        - val_dataset
+        - test_dataset
+        """
         pass
     
     @abstractmethod
     def _set_train_options(self):
-        """sets train options
-    needs to set following variables
-    - self.batch_size
-    - self.epochs
-    - self.callback
-    - self.input_shape
-    - self.labels
-
-
-    """
+        """sets the training options
+        Needs to set following object variables
+        - batch_size
+        - epochs
+        - callback
+        - input_shape
+        - labels
+        - callback
+        """
         pass
     
     @staticmethod
     def classification_report_to_dataframe(y_test, y_pred):
+        """Parses the classification_report 
+
+        Args:
+            y_test (numpy array): Test labels
+            y_pred (numpy array): Predicted labels
+
+        Returns:
+            dataframe: Dataframe with the classification report
+            float: Accuracy
+        """
         report  = classification_report(y_test, y_pred)
         report_data = []
         lines = report.split('\n')
@@ -295,9 +434,23 @@ class Mlflow_controller(ABC):
 
 
 def save_encoder(encoder: OneHotEncoder, filename:str):
+    """Saves OneHotEncoder to file
+
+    Args:
+        encoder (OneHotEncoder): OneHotEncoder to save
+        filename (str): Filename
+    """
     with open(filename, 'wb') as file:
         pickle.dump(encoder, file)
 
 def load_encoder(filename:str) -> OneHotEncoder:
+    """_summary_
+
+    Args:
+        filename (str):Filename
+
+    Returns:
+        OneHotEncoder: Onehotencoder loaded from file
+    """
     pickle_off = open (filename, "rb")
     return pickle.load(pickle_off)
